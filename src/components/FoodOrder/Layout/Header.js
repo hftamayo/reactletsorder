dd1f9c713ac1e7f9f831dd6028c7fc51fd029512
hftamayo <hftamayo@gmail.com>
2022-05-09 11:09:12 -0600

    filtering shoppingCart display based on login

--- src/components/FoodOrder/Layout/Header.js
+++ src/components/FoodOrder/Layout/Header.js
@@ -1,14 +1,16 @@
-import React, { Fragment }from 'react';
+import React, { useContext, Fragment }from 'react';
+import AuthContext from '../store/auth-context';
 import HeaderCartButton from './HeaderCartButton';
 import mealsImage from '../assets/banner.jpg';
 import classes from './Header.module.css';
 
 const Header = props => {
+    const authCtx = useContext(AuthContext);
     return (
         <Fragment>
             <header className={classes.header}>
                 <h1>Lets Order!!!</h1>
-                <HeaderCartButton onClick={props.onShowCart} />
+                {authCtx.isLoggedIn && <HeaderCartButton onClick={props.onShowCart} />}
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImage} alt="all you can eat brunch" />
