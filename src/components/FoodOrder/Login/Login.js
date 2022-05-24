8c931cbc12b50662829aa0139f62f53524810c13
hftamayo <hftamayo@gmail.com>
2022-05-24 14:40:27 -0600

    updating login form to modal part 2

--- src/components/FoodOrder/Login/Login.js
+++ src/components/FoodOrder/Login/Login.js
@@ -10,27 +10,15 @@ const Login = (props) => {
   const [isErrorOnValidate, setIsErrorOnValidate] = useState(false);
   const cartCtx = useContext(CartContext);
 
-  const cartItemRemoveHandler = (id) => {
-    cartCtx.removeItem(id);
+  const validatingHandler = () => {
+    setIsValidating(true);
   };
 
-  const cartItemAddHandler = (item) => {
-    cartCtx.addItem({ ...item, amount: 1 });
+  const errorOnValidateHandler = () => {
+    setIsErrorOnValidate(true);
   };
 
-  const orderHandler = () => {
-    setIsCheckout(true);
-  };
-
-  const showCartHandler = () => {
-    setIsCheckout(false);
-  };
-
-  const errorOnSentOrderHandler = () => {
-    setIsErrorOnSentOrder(true);
-  };
-
-  const submitOrderHandler = async (userData) => {
+  const validateCredentialsHandler = async (userData) => {
     /*
     await fetch("http://localhost:8080/api/orders", {  
       credentials: "include",    
@@ -38,12 +26,12 @@ const Login = (props) => {
       body: JSON.stringify(pruebaData),
       headers: {
         'Content-Type': 'application/json',
-//        'Access-Control-Allow-Origin': '*',
+      //'Access-Control-Allow-Origin': '*',
         'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0MUBmYWtlbWFpbC5jb20iLCJleHAiOjE2MzI3NTAzODYsImlhdCI6MTYzMjcxNDM4Nn0.2tvdnG9B0HdpUpV0xsOKKaATFkyuNVKMpzYE8sXBFtw',
       }
       */
 
-    setIsSubmitting(true);
+    setIsValidating(true);
     const response = await fetch(
       "https://movieserp-default-rtdb.firebaseio.com/orders.json",
       {
@@ -55,84 +43,21 @@ const Login = (props) => {
       }
     );
     if (!response.ok) {
-      errorOnSentOrderHandler();
+      errorOnValidateHandler();
     } else {
-      setIsSubmitting(false);
-      setIsCheckout(false);
-      setDidSubmit(true);
-      cartCtx.clearCart();
+      setIsValidating(false);
+      setIsCanceling(false);
+      setDidValidate(true);
+      //cartCtx.clearCart();
     }
   };
 
-  const cartItems = (
-    <ul className={classes["cart-items"]}>
-      {cartCtx.items.map((item) => (
-        <CartItem
-          key={item.id}
-          name={item.name}
-          amount={item.amount}
-          price={item.price}
-          onRemove={cartItemRemoveHandler.bind(null, item.id)}
-          onAdd={cartItemAddHandler.bind(null, item)}
-        />
-      ))}
-    </ul>
-  );
-
-  const cartContentButtons = (
-    <React.Fragment>
-      <button className={classes["button--alt"]} onClick={props.onClose}>
-        Close
-      </button>
-      <button className={classes["button--alt"]} onClick={orderHandler}>
-        Order's Details
-      </button>
-    </React.Fragment>
-  );
-
-  const orderDetailsButtons = (
-    <React.Fragment>
-      <button className={classes["button--alt"]} onClick={props.onClose}>
-        Close
-      </button>
-      <button className={classes["button--alt"]} onClick={showCartHandler}>
-        Cart's Content
-      </button>
-    </React.Fragment>
-  );
-
-  const modalActions = (
-    <div className={classes.actions}>
-      {!isCheckout && hasItems ? cartContentButtons : orderDetailsButtons}
-    </div>
-  );
-
-  const CartModalContent = (
-    <React.Fragment>
-      {cartItems}
-      <div className={classes.total}>
-        <span>Total Amount</span>
-        <span>{totalAmount}</span>
-      </div>
-      {modalActions}
-    </React.Fragment>
-  );
-
-  const OrderDetailsModalContent = (
-    <React.Fragment>
-      {isCheckout && (
-        <OrderDetails onConfirm={submitOrderHandler} onCancel={props.onClose} />
-      )}
-      {modalActions}
-    </React.Fragment>
-  );
-
-  const isSubmittingModalContent = <p>Sending order data...</p>;
+  const isValidatingModalContent = <p>Validating Credentials...</p>;
   /* incluir transaccion para verificar si es exitoso o hubo algun error */
 
-  const errorOnSentOrderModalContent = (
+  const errorOnValidateModalContent = (
     <React.Fragment>
-      <p>Process failed. An error occurs sending the order!</p>
+      <p>User or Password incorrect, please verify</p>
       <div className={classes.actions}>
         <button className={classes.button} onClick={props.onClose}>
           Close
@@ -142,9 +67,9 @@ const Login = (props) => {
   );  
 
 
-  const didSubmitModalContent = (
+  const didValidateModalContent = (
     <React.Fragment>
-      <p>Successfully sent the order!</p>
+      <p>Creditials verified, welcome!</p>
       <div className={classes.actions}>
         <button className={classes.button} onClick={props.onClose}>
           Close
@@ -153,13 +78,60 @@ const Login = (props) => {
     </React.Fragment>
   );
 
+  const loginButtons = (
+    <React.Fragment>
+      <button className={classes["button--alt"]} onClick={props.onClose}>
+        Close
+      </button>
+      <button className={classes["button--alt"]} onClick={validatingHandler}>
+        Validate
+      </button>
+    </React.Fragment>
+  );
+
+  const modalActions = (
+    <div className={classes.actions}>
+      {!isCanceling ? loginButtons : ""}
+    </div>
+  );
+
+  const LoginModalContent = (
+    <React.Fragment>
+        <Input
+          ref={emailInputRef}
+          id="email"
+          label="E-Mail"
+          type="email"
+          isValid={emailIsValid}
+          value={emailState.value}
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
+        />
+        <Input
+          ref={passwordInputRef}
+          id="paswword"
+          label="Password"
+          type="password"
+          isValid={passwordIsValid}
+          value={passwordState.value}
+          onChange={passwordChangeHandler}
+          onBlur={validatePasswordHandler}
+        />
+        <div className={classes.actions}>
+          <Button type="submit" className={classes.btn}>
+            Login
+          </Button>
+        </div>
+        {modalActions}
+    </React.Fragment>
+  );
+
   return (
     <Modal onClose={props.onClose}>
-      {!isSubmitting && !didSubmit && !isCheckout && CartModalContent}
-      {isCheckout && OrderDetailsModalContent}
-      {isSubmitting && isSubmittingModalContent}
-      {isErrorOnSentOrder && errorOnSentOrderModalContent}
-      {!isSubmitting && didSubmit && didSubmitModalContent}
+      {!isCanceling && !isValidating && !isErrorOnValidate && LoginModalContent}
+       {isValidating && isValidatingModalContent}
+      {isErrorOnValidate && errorOnValidateModalContent}
+      {!isValidating && didValidate && didValidateModalContent}
     </Modal>
   );
 };
