c3ee2e0cc076dcc9b63105b34a25191dc19267e1
hftamayo <hftamayo@gmail.com>
2022-05-30 12:06:17 -0600

    updating login icon

--- src/components/FoodOrder/Login/LoginIcon.js
+++ src/components/FoodOrder/Login/LoginIcon.js
@@ -2,10 +2,18 @@ const LoginIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 20"
-      fill="currentColor"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="#ffffff"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
     >
-      <path d="M3 1a1 1 0 000 2h1.22l.305 1.222a.997.997 0 00.01.042l1.358 5.43-.893.892C3.74 11.846 4.632 14 6.414 14H15a1 1 0 000-2H6.414l1-1H14a1 1 0 00.894-.553l3-6A1 1 0 0017 3H6.28l-.31-1.243A1 1 0 005 1H3zM16 16.5a1.5 1.5 0 11-3 0 1.5 1.5 0 013 0zM6.5 18a1.5 1.5 0 100-3 1.5 1.5 0 000 3z" />
+      <path d="M5.52 19c.64-2.2 1.84-3 3.22-3h6.52c1.38 0 2.58.8 3.22 3" />
+      <circle cx="12" cy="10" r="3" />
+      <circle cx="12" cy="12" r="10" />
     </svg>
   );
 };
