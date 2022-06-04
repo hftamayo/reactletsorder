ba2ced8380791e4e079b93051b1d41a0f19f453a
hftamayo <hftamayo@gmail.com>
2022-06-04 13:11:03 -0600

    disabling autcomplete on inputs (test on incogn)

--- src/components/FoodOrder/UI/Input/Input.js
+++ src/components/FoodOrder/UI/Input/Input.js
@@ -28,6 +28,7 @@ const Input = React.forwardRef((props, ref) => {
           value={props.value}
           onChange={props.onChange}
           onBlur={props.onBlur}
+          autoComplete={props.autodata}
         />
       </div>
     );
