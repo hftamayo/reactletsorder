933b26941476c5b10582b23d20f96e25e4ead7f9
hftamayo <hftamayo@gmail.com>
2022-06-19 18:03:31 -0600

    adjusting availablemeals to heroku endpoint

--- src/components/FoodOrder/Meals/AvailableMeals.js
+++ src/components/FoodOrder/Meals/AvailableMeals.js
@@ -12,8 +12,10 @@ const AvailableMeals = () => {
 
   useEffect(() => {
     const fetchMeals = async () => {
-      const response = await fetch('https://movieserp-default-rtdb.firebaseio.com/meals.json');
-      //const response = await fetch('http://localhost:8080/api/products');
+      //const response = await fetch('https://movieserp-default-rtdb.firebaseio.com/meals.json');
+      const response = await fetch("https://rorletsorder.herokuapp.com/meals", {
+        method: 'GET',
+      });
 
       if(!response.ok){
         throw new Error('something went wrong');
