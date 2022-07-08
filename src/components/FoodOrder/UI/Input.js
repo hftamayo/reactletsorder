cf8dde63999b8f0f21c5406604072907f1fca440
hftamayo <hftamayo@gmail.com>
2022-07-08 12:21:48 -0600

    re-enabling props.input.id to display spinner

--- src/components/FoodOrder/UI/Input.js
+++ src/components/FoodOrder/UI/Input.js
@@ -4,11 +4,11 @@ import classes from './Input.module.css';
 const Input = React.forwardRef((props, ref) => {
     return(
         <div className={classes.input}>
-            <label htmlFor={props.id}>{props.label}</label>
+            <label htmlFor={props.input.id}>{props.label}</label>
             {/* el spread operator es para estar seguro que el
             ultimo valor es qel que se utilizara y que interactua con
             los demas componentes padres */}
-            <input ref={ref} {...props}/>
+            <input ref={ref} {...props.input}/>
         </div>
     );
 });
